7ddf29ece803dd9fcdfb4c5cee2c6de174ba64fc
unknown <lucasphang98@gmail.com>
2020-04-17 13:07:34 +1000

    Improved structure and readability

--- changelog.md
+++ changelog.md
@@ -12,13 +12,15 @@ Here are the steps that I've taken to refactor the code:
 1. Replaced all incrementation and decrementation with `+=` and `-=` respectively
 2. Sulfuras, Hand of Ragnaros is a legendary item that never has to be sold or drop in quality, hence we don't need to process it at all and can skip to the next item
 3. The code increment and decrement the item's quality is repeated several times.
-e.g. ```
-if item.quality < 50
-    item.quality += 1
-end
-```
+    e.g. ```
+    if item.quality < 50
+        item.quality += 1
+    end
+    ```
 Refactored it by creating another function `incrementQuality(item, qualityLimit)` that takes an item and a qualityLimit, and increments the quality of the item if it hasn't reached the limit. Did the same for `decrementQuality`
 4. There's also a few hardcoded strings and numbers in the code. We replace these with Constants
 5. Using DeMorgan's Laws, convert `if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES)` to `item.name == BRIE || item.name == CONCERT_PASS`
 6. Extract the single `item.sell_in -= 1` portion of the code and made it a function, so that its purpose is more elaborate. 
-7. Changed to hardcoded 11 and 6 values in updating the quality value of the backstage passes to 10 and 5. Also, moved the part of the code that updates the sellIn value of items to the top, separating sellin logic with quality logic 
+7. Changed to hardcoded 11 and 6 values in updating the quality value of the backstage passes to 10 and 5. 
+8. Separated nested if-else conditions to a flat if-elsif-else format, improving the structure of the code
+9. created `hasExpired(item)` method to check if an item's sellin value < 0 and refactored the code to accomodate it
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,6 +6,7 @@ class GildedRose
 	LOWER_QLTY_LIMIT = 0
 	CONCERT_QUALITY_DOUBLE_LIMIT = 10
 	CONCERT_QUALITY_TRIPLE_LIMIT = 5
+	CONCERT_EXPIRY_VALUE = 0
 
 	def initialize(items)
 		@items = items
@@ -26,8 +27,13 @@ This function takes an item and a qualityLimit, and increments the quality of th
 		end
 	end
 	# update the sell in value of a particular item
-	def updateSellIn(item)
+	def decrementSellIn(item)
 		item.sell_in -=  1
+	end
+
+	# checks if the item's sellin value is still positive
+	def hasExpired(item)
+		item.sell_in < 0
 	end
 
 	def update_quality
@@ -35,27 +41,32 @@ This function takes an item and a qualityLimit, and increments the quality of th
 			if item.name == SULFURAS
 				next
 			end
-			updateSellIn(item)
 
-			if item.name == BRIE || item.name == CONCERT_PASS
+			decrementSellIn(item)
+			if item.name == BRIE
 				incrementQuality(item, UPPER_QLTY_LIMIT)
-				if item.name == CONCERT_PASS
-					if item.sell_in < CONCERT_QUALITY_DOUBLE_LIMIT
-						incrementQuality(item, UPPER_QLTY_LIMIT)
-					end
-					if item.sell_in < CONCERT_QUALITY_TRIPLE_LIMIT
-						incrementQuality(item, UPPER_QLTY_LIMIT)
-					end
+				# if Brie has expired, increment its quality
+				if hasExpired(item)
+					incrementQuality(item, UPPER_QLTY_LIMIT)
+				end
+			elsif item.name == CONCERT_PASS
+				incrementQuality(item, UPPER_QLTY_LIMIT)
+				# if there are less than CONCERT_QUALITY_DOUBLE_LIMIT days to concert, decrement again
+				if item.sell_in < CONCERT_QUALITY_DOUBLE_LIMIT
+					incrementQuality(item, UPPER_QLTY_LIMIT)
+				end
+				# if there are less thatn CONCERT_QUALITY_TRIPLE_LIMIT days to concert, decrement again
+				if item.sell_in < CONCERT_QUALITY_TRIPLE_LIMIT
+					incrementQuality(item, UPPER_QLTY_LIMIT)
+				end
+				# if concert passes has expired, its quality is 0
+				if hasExpired(item)
+					item.quality = CONCERT_EXPIRY_VALUE
 				end
 			else
 				decrementQuality(item,LOWER_QLTY_LIMIT)
-			end
-			if item.sell_in < LOWER_QLTY_LIMIT
-				if item.name == BRIE
-					incrementQuality(item, UPPER_QLTY_LIMIT)
-				elsif item.name == CONCERT_PASS
-					item.quality -=  item.quality
-				else
+				# if it is a normal item, decrement twice as fast
+				if hasExpired(item)
 					decrementQuality(item,LOWER_QLTY_LIMIT)
 				end
 			end
